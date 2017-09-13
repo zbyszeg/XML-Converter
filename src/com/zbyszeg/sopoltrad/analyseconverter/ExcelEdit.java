f46674567cb444c3ca039ee62549f590556cf522
zbyszeg <zbyszek.gora@gmail.com>
2017-09-13 20:14:20 +0200

    format

--- src/com/zbyszeg/sopoltrad/analyseconverter/ExcelEdit.java
+++ src/com/zbyszeg/sopoltrad/analyseconverter/ExcelEdit.java
@@ -1,9 +1,15 @@
 package com.zbyszeg.sopoltrad.analyseconverter;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,14 +27,41 @@ public class ExcelEdit {
 
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
 			XSSFSheet analyse = workbook.getSheetAt(0);
+			XSSFFont font = workbook.createFont();
 			
-			analyse.autoSizeColumn(1);
-			analyse.autoSizeColumn(2);
-			analyse.autoSizeColumn(3);
-			analyse.autoSizeColumn(4);
-			analyse.autoSizeColumn(5);
-			analyse.autoSizeColumn(6);
+			font.setBold(true);
+			font.setColor(new XSSFColor(new Color(102,0,0)));
+			font.setFontHeight(12);
+			
 
+			CellStyle style = workbook.createCellStyle();
+			CellStyle bold = workbook.createCellStyle();
+			
+			style.setDataFormat(workbook.createDataFormat().getFormat("0.00%"));
+			bold.setFont(font);			
+
+			Cell cell = null;
+			
+			for (Row row : analyse) {
+				cell = row.getCell(5);
+				if(cell!=null)
+					cell.setCellStyle(style);
+			}
+			
+			for (Row row : analyse) {
+				cell = row.getCell(1);
+				if(cell!=null)
+					cell.setCellStyle(bold);
+			}
+			
+			for (int i=2; i<=6; i++) {
+				cell = analyse.getRow(3).getCell(i);
+				cell.setCellStyle(bold);
+			}
+			
+			for (int i = 1; i <= 6; i++)
+				analyse.autoSizeColumn(i);
+			
 			XSSFFormulaEvaluator.evaluateAllFormulaCells((XSSFWorkbook) workbook);
 
 			file.close();
@@ -39,7 +72,7 @@ public class ExcelEdit {
 			workbook.close();
 
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 	}
 }
